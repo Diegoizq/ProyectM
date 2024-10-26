cd2a98bd1f289ef322b30569a9e88707e77a21f9
Cristhian Rodriguez <cristhianp252@gmial.com>
2024-10-25 19:39:00 -0500

    taller pana carlos

---
+++ img/java/javaprofesionales.js
@@ -0,0 +1,60 @@
+const profesionalesP=[
+    {
+    id: 1,
+    profesionales: "Psicologo",
+    precio:100,
+    imagen: "../img/doc1.jpg", 
+
+    },
+
+    {
+        id: 2,
+        profesionales: "Psicologo",
+        precio:100,
+        imagen: "../img/psico1.jpg", 
+    },
+
+    {
+        id: 3,
+        profesionales: "Psicologo",
+        precio:100,
+        imagen: "../img/psico 2.jpg", 
+    },
+
+    {
+        id: 4,
+        profesionales: "Psicologo",
+        precio:100,
+        imagen: "../img/psico3.jpg", 
+    },
+
+    {
+        id: 5,
+        profesionales: "Psicologo",
+        precio:100,
+        imagen: "../img/psico 4.jpg", 
+    },
+
+    {
+         id: 6,
+        profesionales: "Psicologo",
+        precio:100,
+        imagen: "../img/pisco 5.jpg", 
+    },
+
+
+    {
+        id: 7,
+       profesionales: "Psicologo",
+       precio:100,
+       imagen: "../img/pisco6.jpg", 
+   },
+
+
+   {
+    id: 8,
+   profesionales: "Psicologo",
+   precio:100,
+   imagen: "../img/pisco 7.jpg", 
+}
+]
